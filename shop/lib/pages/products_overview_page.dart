b56d5a4273c76d36e64dab09ab439da7b98f21c9
Daniel <avelar42@hotmail.com>
2022-08-14 12:06:39 -0300

    implementacao do favorite

--- shop/lib/pages/products_overview_page.dart
+++ shop/lib/pages/products_overview_page.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../components/product_grid.dart';
+import '../models/product_list.dart';
+
+enum FilterOptions { favorite, all }
 
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Shop'),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.all,
+              )
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showallOnly();
+              }
+            },
+          )
+        ],
       ),
       body: ProductGrid(),
     );
