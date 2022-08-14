b0adaad088e6b2e99a25fd91ea30faa24ea21db1
Daniel <avelar42@hotmail.com>
2022-08-13 23:33:45 -0300

    State implementation

---
+++ shop/lib/components/product_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../components/product_item.dart';
+import '../models/product_list.dart';
+import '../models/product.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+      itemCount: loadedProducts.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+    );
+  }
+}
