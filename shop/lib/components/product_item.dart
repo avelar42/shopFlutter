613d1f92030d847efe8f7e7142ba502db3defdff
Daniel <avelar42@hotmail.com>
2022-08-14 19:13:54 -0300

    Left Product item

--- shop/lib/components/product_item.dart
+++ shop/lib/components/product_item.dart
@@ -23,7 +23,7 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           title: Text(
-            product.title,
+            product.name,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
