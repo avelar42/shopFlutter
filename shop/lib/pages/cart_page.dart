a8e4e0281933cc41da5bed012cd0269fa54389d8
Daniel <avelar42@hotmail.com>
2022-08-15 21:33:04 -0300

    adjust in cart page

--- shop/lib/pages/cart_page.dart
+++ shop/lib/pages/cart_page.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(title: Text('Carrinho')),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(25),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Total",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Chip(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      label: Text(
+                        'R\$${cart.totalAmout}',
+                        style: TextStyle(
+                            color: Theme.of(context)
+                                .primaryTextTheme
+                                .headline6
+                                ?.color),
+                      )),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('Comprar'),
+                    style: TextButton.styleFrom(
+                        textStyle:
+                            TextStyle(color: Theme.of(context).primaryColor)),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
