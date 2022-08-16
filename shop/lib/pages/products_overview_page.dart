598f9766a983a008e129d4804c6b089d25ce55c5
Daniel <avelar42@hotmail.com>
2022-08-15 21:15:11 -0300

    Cart Navigator

--- shop/lib/pages/products_overview_page.dart
+++ shop/lib/pages/products_overview_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/badge.dart';
+import 'package:shop/utils/app_routes.dart';
 import '../components/product_grid.dart';
 import '../models/cart.dart';
 import '../models/product_list.dart';
@@ -45,8 +46,11 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
             },
           ),
           Consumer<Cart>(
-            child:
-                IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+            child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(AppRoutes.CART);
+                },
+                icon: Icon(Icons.shopping_cart)),
             builder: (ctx, cart, child) =>
                 Badge(value: cart.itemsCount.toString(), child: child!),
           ),
