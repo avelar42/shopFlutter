b4e33efe2e378cea1c0f1be305010392f8621d76
Daniel <avelar42@hotmail.com>
2022-08-16 22:32:52 -0300

    Cart implementation

--- shop/lib/components/cart_item.dart
+++ shop/lib/components/cart_item.dart
@@ -7,6 +7,22 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(cartItem.name);
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(child: Text('${cartItem.price}')),
+            ),
+          ),
+          title: Text(cartItem.name),
+          subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quatity}'),
+          trailing: Text('${cartItem.quatity}x'),
+        ),
+      ),
+    );
   }
 }
