651903fd7129b98d6d70442fca48b03d1e9c70eb
Daniel <avelar42@hotmail.com>
2022-09-14 12:03:49 -0300

    Adjust in product form page

--- shop/lib/pages/product_form_page.dart
+++ shop/lib/pages/product_form_page.dart
@@ -9,6 +9,30 @@ class ProductFormPage extends StatefulWidget {
 
 class _ProductFormPageState extends State<ProductFormPage> {
   final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _imageUrlFocus.addListener(UpdateImage);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _priceFocus.dispose();
+    _descriptionFocus.dispose();
+    _imageUrlFocus.removeListener(UpdateImage);
+    _imageUrlFocus.dispose();
+  }
+
+  void UpdateImage() {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,9 +55,46 @@ class _ProductFormPageState extends State<ProductFormPage> {
               decoration: InputDecoration(labelText: 'Preco'),
               textInputAction: TextInputAction.next,
               focusNode: _priceFocus,
+              onFieldSubmitted: (_) {
+                FocusScope.of(context).requestFocus(_descriptionFocus);
+              },
               keyboardType: TextInputType.numberWithOptions(
                 decimal: true,
               ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Descricao'),
+              focusNode: _descriptionFocus,
+              keyboardType: TextInputType.multiline,
+              maxLines: 3,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Url da Imagem'),
+                    focusNode: _imageUrlFocus,
+                    keyboardType: TextInputType.url,
+                    textInputAction: TextInputAction.done,
+                    controller: _imageUrlController,
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.only(top: 10, left: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1)),
+                  alignment: Alignment.center,
+                  child: _imageUrlController.text.isEmpty
+                      ? Text('Informe a url')
+                      : FittedBox(
+                          child: Image.network(_imageUrlController.text),
+                          fit: BoxFit.cover,
+                        ),
+                )
+              ],
             ),
           ],
         )),
