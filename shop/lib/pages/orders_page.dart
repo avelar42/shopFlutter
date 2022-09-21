2f44256b6a29a506e6bb0b548c86fb82a3fbc780
Daniel <avelar42@hotmail.com>
2022-09-21 15:54:04 -0300

    Get orders from server

--- shop/lib/pages/orders_page.dart
+++ shop/lib/pages/orders_page.dart
@@ -5,8 +5,26 @@ import 'package:shop/components/order.dart';
 
 import '../models/order_list.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
   const OrdersPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Provider.of<OrderList>(context, listen: false).loadOrders().then((_) {
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +34,11 @@ class OrdersPage extends StatelessWidget {
         title: Text('Meus pedidos'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-          itemCount: orders.itemsCount,
-          itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i])),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: orders.itemsCount,
+              itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i])),
     );
   }
 }
