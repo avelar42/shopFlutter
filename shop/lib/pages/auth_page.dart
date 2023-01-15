12671400df2b855c39409378f9896a4cc7c3ae16
Daniel <avelar42@hotmail.com>
2023-01-15 11:04:02 -0300

    Correcao de responsividade

--- shop/lib/pages/auth_page.dart
+++ shop/lib/pages/auth_page.dart
@@ -18,35 +18,42 @@ class AuthPage extends StatelessWidget {
               Color.fromRGBO(255, 188, 117, .9),
             ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 70),
-                  //tip: Using CascadeOperator
-                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                  decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                            blurRadius: 8,
-                            color: Colors.black26,
-                            offset: Offset(0, 2))
-                      ],
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.deepOrange.shade900),
-                  child: Text(
-                    'Minha Loja',
-                    style: TextStyle(
-                        fontSize: 45, fontFamily: 'Anton', color: Colors.white),
-                  ),
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 70),
+                      //tip: Using CascadeOperator
+                      transform: Matrix4.rotationZ(-8 * pi / 180)
+                        ..translate(-10.0),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 8,
+                                color: Colors.black26,
+                                offset: Offset(0, 2))
+                          ],
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.deepOrange.shade900),
+                      child: Text(
+                        'Minha Loja',
+                        style: TextStyle(
+                            fontSize: 45,
+                            fontFamily: 'Anton',
+                            color: Colors.white),
+                      ),
+                    ),
+                    AuthForm()
+                  ],
                 ),
-                AuthForm()
-              ],
+              ),
             ),
           )
         ],
