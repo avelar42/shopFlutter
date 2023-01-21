012f892abcd8b84c8743e7cbce7becfeeeb88257
Daniel <avelar42@hotmail.com>
2023-01-20 23:14:05 -0300

    ajuste na tela de pedidos animacoes

--- shop/lib/components/order.dart
+++ shop/lib/components/order.dart
@@ -16,25 +16,29 @@ class _OrderWidgetState extends State<OrderWidget> {
   bool _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
-            trailing: IconButton(
-                onPressed: () {
-                  setState(() {
-                    _expanded = !_expanded;
-                  });
-                },
-                icon: Icon(Icons.expand_more)),
-          ),
-          if (_expanded)
-            Container(
+    final itemsHeight = (widget.order.products.length * 25.0) + 10;
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded ? itemsHeight + 80 : 80,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+              subtitle: Text(
+                  DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
+              trailing: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _expanded = !_expanded;
+                    });
+                  },
+                  icon: Icon(Icons.expand_more)),
+            ),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              height: _expanded ? itemsHeight : 0,
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: widget.order.products.length * 25 + 10,
               child: ListView(
                   children: widget.order.products.map((product) {
                 return Row(
@@ -46,14 +50,15 @@ class _OrderWidgetState extends State<OrderWidget> {
                           TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      '${product.quatity}x R\$${product.price}',
+                      '${product.quatity}x R\$ ${product.price}',
                       style: TextStyle(fontSize: 18, color: Colors.grey),
                     )
                   ],
                 );
               }).toList()),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
